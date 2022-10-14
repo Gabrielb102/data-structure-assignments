a30c356b41a9067d53d8c072b1248527e095c8af
Gabriel <gabrielb102@gmail.com>
2022-10-14 11:56:01 -0700

    finished divide and conquer assignment

--- Unit47.3-prob-divide-conquer/find-rotated-index.js
+++ Unit47.3-prob-divide-conquer/find-rotated-index.js
@@ -2,47 +2,60 @@ function average (r, l) {
     return Math.floor((r + l) / 2);
 }
 
-function findRotatedIndex(array, num) {
-    
-    let sortedArr = array;
+function findTarget(array, target) {
+    let r = array.length - 1;
+    let l = 0;
+    let m = average(r,l);
+
+    while (l <= r) {
+        if (array[m] < target) {
+            l = m + 1;
+            m = average(l, r)
+            continue;
+        } else if (array[m] > target) {
+            r = m - 1;
+            m = average(r,l);
+            continue;
+        } else {
+            return m;
+        }   
+    }
+    return -1;
+}
+
+function getOffsetReturnSorted(array) {
     let l = 0;
     let r = array.length - 1;
-    let p
-    
-    if (array.length === 1) {
-        return num === array[0] ? 0 : -1;
-    }
+    let m = average(l, r);
 
-    if (array[0] > array[array.length -1]) {
-        while (l < r) {
-            p = average(l,r);
-            if (array[p] > array[p + 1]) {
-                p++;
-                break;
-            }
-            if (array[p] > array[l]) {
-                l = p + 1;
-            }
-            if (array[p] > array[r]) {
-                r = p - 1;
-            }
-        }
-        sortedArr = [...array.slice(p, array.length), ...array.slice(0,p)]
+    if (array[r] > array[l]) {
+        return {sortedArr : array, offset: 0};
     }
 
     while (l <= r) {
-        let m = average(r,l);
-        if (sortedArr[m] === num) {
-            return m;
-        } else if (sortedArr[m] > num) {
-            r = m - 1;
-        } else if (sortedArr[m] < num) {
+        if (array[m] > r ) {
             l = m + 1;
-        }
-        return -1;
+            m = average(l, r)
+            continue;
+        } else if (array[m] < l) {
+            r = m - 1;
+            m = average(r,l);
+            continue;
+        }  
     }
+    return {sortedArr : [...array.slice(m + 1), ...array.slice(0, m + 1)], offset : m + 1};
 
-    return -1;
+}
+
+function findRotatedIndex(array, target) {
+    const {sortedArr, offset} = getOffsetReturnSorted(array);
+    const idx = findTarget(sortedArr, target)
+    if (idx === -1) return -1;
+    let result = idx - offset;
+    if (result < 0) {
+        result += array.length;
+    }
+    return result;
 }
 
 module.exports = findRotatedIndex
--- Unit47.3-prob-divide-conquer/find-rotation-count.js
+++ Unit47.3-prob-divide-conquer/find-rotation-count.js
@@ -1,5 +1,35 @@
-function findRotationCount() {
+function average(r, l) {
+    return Math.floor((r + l) / 2);
+  }
   
+function findRotationCount(array) {
+
+let l = 0;
+let r = array.length - 1;
+let m = average(l, r);
+
+if (array[r] > array[l]) {
+    return 0;
 }
 
-module.exports = findRotationCount
+while (l <= r) {
+
+    if (array[m] > r) {
+    l = m + 1;
+    m = average(l, r)
+    continue;
+    } else if (array[m] < l) {
+    r = m - 1;
+    m = average(r, l);
+    continue;
+    } else {
+    break
+    }
+}
+return m;
+
+}
+
+const array1 = [34, 56, 78, 0, 2, 3];
+
+module.exports = findRotationCount;  
